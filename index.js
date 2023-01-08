beba9e53afcb5a95d6d1bae347be40b7f80af53b
Dan <danaaboston@gmail.com>
2023-01-08 14:34:12 -0500

    Further updated index.js to create new Intern objects using the classes from the lib directory. Updated classes to properly call from the Employee class

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Intern = require('./lib/Intern.js');
+const Engineer = require('./lib/Engineer.js');
+const Manager = require('./lib/Manager.js');
 
 function buildTeam () {
     inquirer.prompt([
@@ -7,19 +10,56 @@ function buildTeam () {
             type: 'list',
             message: 'Which role would you like to add?',
             choices: ['Intern', 'Engineer', 'Manager', 'None - all employees accounted for.'],
-            name: 'addEmployee'
+            name: 'employeeType'
         }
     ])
     .then((data) => {
-    if (data.addEmployee === 'Intern') {
-        console.log('INTERN');
-    } else if (data.addEmployee === 'Engineer') {
-        console.log('ENGINEER');
-    } else if (data.addEmployee === 'Manager') {
-        console.log('MANAGER');
+    if (data.employeeType === 'Intern') {
+        addIntern(data);
+    } else if (data.employeeType === 'Engineer') {
+        addEngineer(data);
+    } else if (data.employeeType === 'Manager') {
+        addManager(data);
     } else {
         console.log('COMPLETE');
     }}
 )};
+
+function addIntern(data) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is the intern's name?`,
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: `What is the intern's employee ID?`,
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `What is the intern's email address?`,
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: `What school does the intern attend?`,
+            name: 'school'
+        }
+    ])
+    .then((response) => {
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        console.log(intern);
+    })
+};
+
+function addEngineer(data) {
+    console.log(data);
+};
+
+function addManager(data) {
+    console.log(data);
+};
 
 buildTeam();
